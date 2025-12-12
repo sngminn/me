bd4c65193545553e76355c7ab00746528b75b826
solmin <smin2020@icloud.com>
2025-12-12 23:31:06 +0900

    feat: 랜딩 페이지 소개 문구 및 버튼 추가
    * 페이지 상단에 자기소개 문구를 추가했습니다.
    * 'JavaScript' 버튼과 화살표 아이콘을 추가했습니다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from 'lucide-react';
 import { Sidebar } from '@/src/components/layout/Sidebar';
 import { getAllPosts } from '@/src/lib/obsidian/post';
 
@@ -11,6 +12,13 @@ export default function Home() {
         <br />
         김승민입니다_
       </h1>
+      <span>개발, 디자인 이것저것 만듭니다</span>
+      <button
+        type="button"
+        className="text-base flex justify-center items-center bg-white rounded-full border-2 border-primary-bg text-text-inverse px-4 py-1 font-semibold"
+      >
+        JavaScript <ArrowRight size={16} />
+      </button>
       <Sidebar posts={allPosts} />
     </div>
   );
