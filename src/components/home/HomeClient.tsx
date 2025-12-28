5dc1e2df8d7ece30b1a50308c4370a0eeffeefc4
solmin <smin2020@icloud.com>
2025-12-29 02:45:29 +0900

    chore: code update

--- src/components/home/HomeClient.tsx
+++ src/components/home/HomeClient.tsx
@@ -54,25 +54,26 @@ function FooterBelt({ type }: { type: footerType }) {
 function HeroSection({ isScrolled }: { isScrolled: boolean }) {
   return (
     <div className="fixed top-0 left-1/2 -translate-x-1/2 max-w-[1080px] w-full h-full overflow-hidden">
-      {isScrolled ? (
-        <div className="flex flex-col gap-18 mt-[45vh]">
-          {Object.keys(footers).map((type) => (
-            <FooterBelt key={type} type={type as footerType} />
-          ))}
+      <div
+        className={`${isScrolled ? 'opacity-100' : 'opacity-0'} absolute flex flex-col gap-18 mt-[45vh]`}
+      >
+        {Object.keys(footers).map((type) => (
+          <FooterBelt key={type} type={type as footerType} />
+        ))}
+      </div>
+      <div
+        className={`${isScrolled ? 'opacity-0' : 'opacity-100'} absolute flex-col h-[70vh] w-full justify-end items-start`}
+      >
+        <AsciiVideo className="absolute top-0 -right-[250px]" />
+        <div className="flex flex-col gap-6 p-8 h-full justify-end">
+          <h1 className='text-primary-bg text-4xl whitespace-nowrap font-extrabold font-["SUITE"]'>
+            {`<프론트엔드 />개발자`}
+            <br />
+            {`{김승민}입니다_`}
+          </h1>
+          <span className="text-primary-bg">개발, 디자인 이것저것 만듭니다</span>
         </div>
-      ) : (
-        <div className="flex-col h-[70vh] w-full justify-end items-start relative">
-          <AsciiVideo className="absolute top-0 -right-[250px]" />
-          <div className="flex flex-col gap-6 p-8 h-full justify-end">
-            <h1 className='text-primary-bg text-4xl whitespace-nowrap font-extrabold font-["SUITE"]'>
-              {`<프론트엔드 />개발자`}
-              <br />
-              {`{김승민}입니다_`}
-            </h1>
-            <span className="text-primary-bg">개발, 디자인 이것저것 만듭니다</span>
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
@@ -82,7 +83,7 @@ export default function HomeClient({ allPosts }: { allPosts: Post[] }) {
 
   useEffect(() => {
     function handleScroll() {
-      const threshold = window.innerHeight * 1;
+      const threshold = window.innerHeight * 0.7;
       setIsScrolled(window.scrollY > threshold);
     }
     handleScroll();
