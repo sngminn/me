3f4f2b6831e514f0de5fb8b3f1093380d9966243
solmin <smin2020@icloud.com>
2025-12-30 15:05:32 +0900

    refactor: Improve scroll detection logic
    - Replaced manual scroll event listener with Intersection Observer for more efficient scroll tracking.

--- src/components/posts/Content.tsx
+++ src/components/posts/Content.tsx
@@ -1,23 +1,28 @@
 'use client';
 
 import Image from 'next/image';
-import { type ReactNode, useEffect, useState } from 'react';
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 import { Tag } from '@/src/components/ui/Tag';
 import type { Post } from '@/src/lib/obsidian/types';
 import { longDate } from '@/src/lib/utils/day';
 
-// TODO: 지금 이 방식(children으로 Mdx 넘겨주기) 맞는지 확인
 export default function Content({ post, children }: { post: Post; children: ReactNode }) {
   const [scrolled, setScrolled] = useState<boolean>(false);
-  useEffect(() => {
-    function handleScroll() {
-      const threshold = window.innerHeight * 0.5;
-      setScrolled(window.scrollY > threshold);
-    }
-    window.addEventListener('scroll', handleScroll);
+  const sentinelRef = useRef<HTMLHeadingElement | null>(null);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+  useEffect(() => {
+    if (!sentinelRef.current) return;
+
+    const observer = new IntersectionObserver((entry) => {
+      const target = entry[0];
+      setScrolled(!target.isIntersecting);
+    });
+
+    observer.observe(sentinelRef.current);
+
+    return () => observer.disconnect();
   }, []);
+
   return (
     <>
       <div
@@ -36,7 +41,9 @@ export default function Content({ post, children }: { post: Post; children: Reac
           />
         </div>
         <Tag text={post.tags[0]} />
-        <h1 className="font-semibold text-2xl leading-[125%] text-text-highlight">{post.title}</h1>
+        <h1 ref={sentinelRef} className="font-semibold text-2xl leading-[125%] text-text-highlight">
+          {post.title}
+        </h1>
         <span className="text-xs">{longDate(post.date)}</span>
         <hr className="text-bg-subtle" />
         <div className="prose dark:prose-invert w-full mt-8">{children}</div>
